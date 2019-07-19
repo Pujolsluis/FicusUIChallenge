6f036404d3e36ae5aa54df650af834a5be9bf7f1
Luis Pujols <luispujols@hotmail.com>
2019-07-19 11:16:58 -0400

    Added the TimeLine Gradient and green leafs line

--- FicusUIChallenge/FicusUIChallenge/ViewModels/TimelinePageViewModel.cs
+++ FicusUIChallenge/FicusUIChallenge/ViewModels/TimelinePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
 
@@ -6,8 +7,16 @@ namespace FicusUIChallenge.ViewModels
 {
     public class TimeLinePageViewModel : BaseViewModel
     {
+        public DelegateCommand OnBackCommand { get; set; }
         public TimeLinePageViewModel(INavigationService navigationService, IPageDialogService pageDialogService = null) : base(navigationService, pageDialogService)
         {
+            OnBackCommand = new DelegateCommand(async () =>
+           {
+               if (IsBusy) return;
+               IsBusy = true;
+               await NavigationService.GoBackAsync();
+               IsBusy = false;
+           });
         }
     }
 }
