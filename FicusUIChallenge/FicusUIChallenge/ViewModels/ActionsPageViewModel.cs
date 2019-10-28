aa0cca53820e24685be05d0eff05327445dc68a6
Luis Pujols <luispujols@hotmail.com>
2019-10-27 21:34:18 -0400

    Added the pt2 gif and polished ActionsPage UI

--- FicusUIChallenge/FicusUIChallenge/ViewModels/ActionsPageViewModel.cs
+++ FicusUIChallenge/FicusUIChallenge/ViewModels/ActionsPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
 
@@ -7,9 +8,17 @@ namespace FicusUIChallenge.ViewModels
     public class ActionsPageViewModel : BaseViewModel
     {
 
+        public DelegateCommand OnBackCommand { get; set; }
 
         public ActionsPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService = null) : base(navigationService, pageDialogService)
         {
+            OnBackCommand = new DelegateCommand(async () =>
+            {
+                if (IsBusy) return;
+                IsBusy = true;
+                await NavigationService.GoBackAsync();
+                IsBusy = false;
+            });
         }
     }
 }
